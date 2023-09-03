2204cc05d9cbea282e1a067c3ba758564b3e4bf1
Daniel Major <daniel.alves@skytef.com.br>
2023-09-03 11:21:02 -0300

    Testando class Product

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,34 +1,32 @@
 import OrderItem from "./order_item";
 
 export default class Order {
+  private _id: string;
+  private _customerId: string;
+  private _items: OrderItem[] = [];
+  private _total: number;
 
-    private _id: string;
-    private _customerId: string;
-    private _items: OrderItem[] = [];
-    private _total: number;
+  constructor(id: string, customerId: string, items: OrderItem[]) {
+    this._id = id;
+    this._customerId = customerId;
+    this._items = items;
+    this._total = this.total();
+    this.validate();
+  }
 
-    constructor(id: string, customerId: string, items: OrderItem[]) {
-        this._id = id;
-        this._customerId = customerId;
-        this._items = items;
-        this._total = this.total();
-        this.validate();
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
     }
-
-    validate() {
-        if (this._id.length === 0) {
-          throw new Error("Id is required");
-        }
-        if (this._customerId.length === 0) {
-            throw new Error("customerId is required");
-        }
-        if (this._items.length === 0) {
-            throw new Error("Items are required");
-        }
-      }
-
-    total(): number {
-        return this._items.reduce((acc, item) => acc + item._price, 0);
+    if (this._customerId.length === 0) {
+      throw new Error("customerId is required");
     }
-    
+    if (this._items.length === 0) {
+      throw new Error("Items are required");
+    }
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
+  }
 }
